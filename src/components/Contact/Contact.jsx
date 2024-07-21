631bb2889f7cbef03ac6b302e99087efad2535ea
Benjamín <benser22@hotmail.com>
2024-07-20 22:57:31 -0300

    refactor(landing): links changed to Contact elements

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,27 +6,27 @@ const Contact = () => {
   const { text, desc, img, button } = data['section-two'];
 
   return (
-    <section className='py-[150px] flex flex-col gap-5'>
+    <section className="py-[150px] flex flex-col gap-5">
       <div
-        className='rounded-md bg-custom-neutral text-custom-white p-5
+        className="rounded-md bg-custom-neutral text-custom-white p-5
       flex flex-col gap-4 items-center justify-around
-      '
+      "
       >
         <img
           src={img.url}
           alt={img.alt}
           title={img.title}
-          className='hidden md:block rounded-md object-cover w-full h-[350px]'
+          className="hidden md:block rounded-md object-cover w-full h-[350px]"
         />
-        <div className='grid place-items-center'>
-          <h2 className='text-[40px] font-semibold text-center'>{text}</h2>
-          <ul className='text-center flex flex-col gap-8 my-10'>
+        <div className="grid place-items-center">
+          <h2 className="text-[40px] font-semibold text-center">{text}</h2>
+          <ul className="text-center flex flex-col gap-8 my-10">
             {desc.map((p) => (
-              <li className='mx-auto w-[90%]'>{p}</li>
+              <li className="mx-auto w-[90%]">{p}</li>
             ))}
           </ul>
-          <CustomButton variant='primary'>
-            <a title={button.title} href={button.href}>
+          <CustomButton variant="primary">
+            <a title={button.title} target="_blank" href={button.href}>
               {button.text}
             </a>
           </CustomButton>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,11 @@
-import CustomButton from "../ui/CustomButton";
-import CustomLink from "../ui/CustomLink";
+import CustomButton from '../ui/CustomButton';
+import CustomLink from '../ui/CustomLink';
 
-import data from "../../data.json";
-import SocialButtons from "../Socials";
+import data from '../../data.json';
+import SocialButtons from '../Socials';
 const Footer = () => {
-  const { text, button } = data["section-footer"];
-  const { menuLinks } = data["section-nav"];
+  const { text, button } = data['section-footer'];
+  const { menuLinks } = data['section-nav'];
   return (
     <footer
       className="footer footer-center p-10 bg-custom-neutral text-custom-white rounded
@@ -14,7 +14,11 @@ const Footer = () => {
       <nav className="flex flex-col gap-4 md:flex-row md:justify-between w-full">
         <p className="text-[25px]">{text}</p>
         <CustomButton variant="primary">
-          <CustomLink href={button.href} title={button.title} />
+          <CustomLink
+            href={button.href}
+            isExternal={button.isExternal}
+            title={button.title}
+          />
         </CustomButton>
       </nav>
       <nav className="flex flex-wrap justify-center gap-5">
